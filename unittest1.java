83e273c798ae1936c6649747a3691fa53437b5f0
Asad Qureshi <asad_qureshee@hotmail.com>
2015-03-04 20:44:15 +0500

    Create unittest1.java

---
+++ unittest1.java
@@ -0,0 +1,12 @@
+package lab4;
+
+import java.io.IOException;
+
+public class unittest1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		client.run();
+	}
+
+}
