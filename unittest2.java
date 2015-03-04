ae63533dcc39d723b7383c9ffc36e9b59742fcc8
Asad Qureshi <asad_qureshee@hotmail.com>
2015-03-04 20:45:51 +0500

    Create unittest2.java

---
+++ unittest2.java
@@ -0,0 +1,12 @@
+package lab4;
+
+import java.io.IOException;
+
+public class unittest2 {
+
+	public static void main(String[] args) throws IOException {
+		server.run("/Users/asadqureshi/Downloads/lab3sample.txt");
+
+	}
+
+}
